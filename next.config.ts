402d649fd8eca3ac6f73fcf784e54953387e600e
Kalpana Joyce Dvari <dovarikalpanajoyce@gmail.com>
2026-04-14 15:03:24 +0100

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,1 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
-}
-
-export default nextConfig
+import type { NextConfig } from `next``r`nconst nextConfig: NextConfig = {}`r`nexport default nextConfig
